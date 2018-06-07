79b038a896e03b2da539aad49b11f3506a202fdc
Christopher Myhill <chris.myhill@rocksolidknowledge.com>
2018-06-07 15:53:09 +0100

    Works with Appium

---
+++ AutmationIdTest/MainPage.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AppiumFormListView
+{
+    public partial class MainPage : ContentPage, INotifyPropertyChanged
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            BindingContext = new MyViewModel();
+            Title = "Blah";
+        }
+    }
+
+    public class MyViewModel : INotifyPropertyChanged
+    {
+        List<MyObject> _allTheData = new List<MyObject>();
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public MyViewModel()
+        {
+            Task.Delay(TimeSpan.FromSeconds(3)).ContinueWith((arg) =>
+            {
+                _allTheData = new List<MyObject>{
+                new MyObject { AValue = "sdklsdkjfh"},
+                new MyObject { AValue = "sdkfh"},
+                new MyObject { AValue = "sdkfxdfsdfjkhh"},
+            };
+                Device.BeginInvokeOnMainThread(() =>
+                {
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(AllTheData)));
+                });
+            });
+        }
+
+        public List<MyObject> AllTheData => _allTheData;
+    }
+
+    public class MyObject
+    {
+        public string AValue
+        {
+            get;
+            set;
+        }
+    }
+}
